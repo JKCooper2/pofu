642baa635532beec179c3d02e75b3c465ab4b29d
JKCooper2 <jesse_cooper@hotmail.com>
2016-07-29 15:02:53 +1000

    Added game display on user home page

--- game/models.py
+++ game/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class GamesManager(models.Manager):
+    def games_for_user(self, user):
+        return super(GamesManager, self).get_queryset().filter(
+            models.Q(player__user_id=user.id))
+
+
 class Game(models.Model):
     status = models.CharField(max_length=1, default='A', choices=[('A', 'Active'), ('I', 'Inactive')])
     title = models.CharField(max_length=50)
+
+    objects = GamesManager()
 
     def __str__(self):
         return "Game " + str(self.id)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from game.models import Game
+
 
 @login_required
 def home(request):
-    return render(request, 'users/home.html')
+    my_games = Game.objects.games_for_user(request.user)
+
+    context = {'my_games': my_games}
+    return render(request, 'users/home.html', context)
