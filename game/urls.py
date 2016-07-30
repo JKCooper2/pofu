f5667ed06607cdb92932fb85d2483bdcbf1d66a8
JKCooper2 <jesse_cooper@hotmail.com>
2016-07-30 12:15:05 +1000

    Added join games function with auto-join for game host

--- game/urls.py
+++ game/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "game"
 urlpatterns = [
     url(r'^setup$', views.setup_game, name='setup'),
-    url(r'^join$', views.join_game, name='join'),
+    url(r'^join$', views.join, name='join'),
+    url(r'^join/(?P<pk>\d+)/$', views.join_game, name='join_game'),
     url(r'^delete/(?P<pk>\d+)/$', views.delete_game, name='delete'),
 ]
--- game/views.py
+++ game/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 
-from .models import Setup
+from .models import Setup, Invitation
 from .forms import SetupGameForm
 
 
@@ -13,7 +13,11 @@ def setup_game(request):
         form = SetupGameForm(data=request.POST, instance=setup)
 
         if form.is_valid():
-            form.save()
+            setup = form.save()
+
+            invite = Invitation(setup=setup, user=request.user)
+            invite.save()
+
             return redirect('users:home')
 
     else:
@@ -36,9 +40,18 @@ def delete_game(request, pk):
 
 
 @login_required
-def join_game(request):
+def join(request):
     games = Setup.objects.all()
-
     context = {'games': games}
-
     return render(request, 'game/join_game.html', context)
+
+
+@login_required
+def join_game(request, pk):
+    setup = get_object_or_404(Setup, pk=pk)
+
+    if request.user not in [inv.user for inv in setup.invitation_set.all()]:
+        invite = Invitation(setup=setup, user=request.user)
+        invite.save()
+
+    return redirect('users:home')
