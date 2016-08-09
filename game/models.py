39bf3d6d56f7fb109ce85026f0edab9d87eb8f26
JKCooper2 <jesse_cooper@hotmail.com>
2016-08-09 13:55:51 +1000

    Refactored Game object

--- game/models.py
+++ game/models.py
@@ -4,33 +4,35 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 import random
-import numpy as np
 
 from cards.models import Deck, Hand, Card
 
 
 class GamesManager(models.Manager):
+    """
+    GamesManager is used for performing queries over all game objects
+    """
     def games_for_user(self, user):
-        return super(GamesManager, self).get_queryset().filter(
-            models.Q(player__user_id=user.id))
+        return super(GamesManager, self).get_queryset().filter(models.Q(player__user_id=user.id))
 
     @staticmethod
     def create_game(setup):
+        """
+        Creates a Game based on a Setup
+
+        Parameters:
+            setup - Complete Setup Instance
+        """
         game = Game(host=setup.host)
         game.save()
 
         invites = setup.invitation_set.all()
 
-        shuffle_order = list(range(len(invites)))
-        random.shuffle(shuffle_order)
-
-        for i, invite in enumerate(invites):
-            player = Player(game=game, user=invite.user, position=shuffle_order[i])
+        for invite in invites:
+            player = Player(game=game, user=invite.user)
             player.save()
 
         setup.delete()
-
-        return game
 
 
 GAME_STATUS = (
@@ -41,51 +43,65 @@ GAME_STATUS = (
 
 
 class Game(models.Model):
+    """
+    Game is responsible for managing the flow of play
+
+    Fields:
+        status - A: Active, F: Finished, C: Cancelled
+        host - User hosting the game (grants privileges to this user)
+
+        order - List of player positions in order of turn, e.g. [2, 3, 0, 1]
+        turn - Holds the index of the current turn in the order. Current turn is player with position order[turn]
+        card_face - Face the lead card was played. 0: down, 1: up, 2: unset
+
+    Related Fields:
+        player - Foreign Key from player to game
+    """
     status = models.CharField(max_length=1, default='A', choices=GAME_STATUS)
-    title = models.CharField(max_length=50, blank=True)
     host = models.ForeignKey(User)
-    deck = Deck()
-    turn = models.IntegerField(default=0)
-    card_face = models.IntegerField(default=2)  # 0 - down, 1 - up, 2 - unset
-    round_end = models.BooleanField(default=True)
+
     order = models.CharField(max_length=40, blank=True)
+    turn = models.IntegerField(default=0)
+    card_face = models.IntegerField(default=2)
 
     objects = GamesManager()
 
     def __str__(self):
         return "Game " + str(self.id)
 
-    def start(self):
-        all_players = self.player_set.all()
+    def is_round_end(self):
+        return self.turn >= self.player_set.count()
 
-        for player in all_players:
-            player.reset()
+    def join_order(self, order, split=None):
+        """
+        Helper function for converting between lists and strings in order to store in DB
+        :param order: List of values
+        :param split: Index of start point (wraps around)
+        :return: string of list joined by ','
+        """
+        order0 = list(range(len(order)))
 
-        self.deck.deal(all_players)
+        if split is None:
+            self.order = ','.join(map(str, order0))
 
-        self.turn = 0
-        self.order = ','.join(map(str, list(range(len(all_players)))))
-        self.save()
+        else:
+            order1 = order0[split:] + order0[:split]
+            self.order = ','.join(map(str, list(order1)))
 
-        first = all_players.get(position=0)
-        first.turn = True
-        first.save()
-
-        self.start_round()
-
-    def start_round(self):
-        all_players = self.player_set.all()
-
-        if not all([player.ready for player in all_players]):
-            print("Not all players ready to start next round")
-            return
-
-        # Reset turns
-        self.card_face = 2
-        self.round_end = False
         self.save()
 
     def poll(self, user):
+        """
+        JS polls server every few seconds to check for updates to the game status
+        Could add step to game and only return html if step > what is passed through
+        Returned HTML currently only a few KB though
+
+        Parameters:
+            user - User instance
+
+        Returns:
+            Dictionary containing html of all players
+        """
         all_players = self.player_set.all()
 
         player = all_players.get(user=user)
@@ -98,65 +114,103 @@ class Game(models.Model):
         return {'self': player_html,
                 'players': other_html}
 
-    def end_round(self):
+    def start(self):
+        """
+        Starts a new game
+        """
         all_players = self.player_set.all()
 
-        # Flip cards if necessary
-        for player in all_players:
-            player.action.face_up = True
-            player.action.save()
-            player.ready = False    # Used to trigger next round deal
-            player.save()
+        # Shuffle positions
+        positions = list(range(len(all_players)))
+        random.shuffle(positions)
 
-        # Determine winner
+        for i, player in enumerate(all_players):
+            turn = positions[i] == 0
+            player.reset(position=positions[i], turn=turn)
+
+        # Deal out cards
+        Deck().deal(all_players)
+
+        # Store turn order used [0, 1, 2, 3, 4, 5]
+        self.join_order(all_players)
+
+        self.turn = 0
+        self.save()
+
+        self.start_round()
+
+    def start_round(self):
+        """
+        Checks if all players are ready and if so starts the game round
+        by setting the turn to be 0
+        """
+        if not all([player.ready for player in self.player_set.all()]):
+            return
+
+        # Reset turns
+        self.card_face = 2
+        self.turn = 0
+        self.save()
+
+    def next_turn(self):
+        """
+        Decides whether to end the round or to update the currently active player
+        """
+        self.turn += 1
+        self.save()
+
+        if self.is_round_end():
+            self.end_round()
+            return
+
+        # Set face-up based on first player so other players in the round must follow play
+        if self.card_face == 2:
+            self.card_face = int(self.player_set.get(position=0).action.face_up)
+            self.save()
+
+        # Set next players turn to be active
+        self.player_set.get(position=self.order.split(',')[self.turn]).set_turn(True)
+
+    def score_bonus(self, player):
+        # Bonus of 2 points if first player plays face up
+        if player.position == self.order.split(',')[0] and self.card_face == 1:
+            return 2
+
+        return 0
+
+    def find_round_winner(self, players):
+        """
+        Returns Player with best cards for that round
+        """
         best_score = 0
         best_player = None
 
-        for player in all_players:
-            score = player.hand_score()
+        for player in players:
+            player.end_round()
+            score = player.hand_score() + self.score_bonus(player)
 
-            print("FIRST PLAY:", player.position, self.order.split(',')[0], self.card_face)
-
-            if player.position == int(self.order.split(',')[0]) and self.card_face == 1:
-                print(player.user.username, " GETS BONUS")
-                score += 2
-
+            # Score has to beat current best in order to win
             if score > best_score:
                 best_score = score
                 best_player = player
 
-        # Add points
+        return best_player
+
+    def end_round(self):
+        """
+        Scores the round, gives points to winner, alters order
+        """
+        all_players = self.player_set.all()
+
+        round_winner = self.find_round_winner(all_players)
+
+        # Give winner points
         points = sum([player.hand_points() for player in all_players])
-        best_player.points += points
-        best_player.turn = True  # Round winner starts next hand
-        best_player.save()
+        round_winner.won_round(points)
 
-        # Display button to move to next round
+        # Alter order so round winner goes first
+        self.join_order(all_players, round_winner.position)
         self.card_face = 2
-        self.round_end = True
-
-        order0 = list(range(len(all_players)))
-        order1 = order0[best_player.position:] + order0[:best_player.position]
-
-        self.order = ','.join(map(str, list(order1)))
-        self.save()
-
-    def next_turn(self):
-        self.turn += 1
-
-        # Set face-up so other players in the round must follow play
-        if self.card_face == 2:
-            self.card_face = int(self.player_set.get(position=0).action.face_up)
-
-        if self.turn >= self.player_set.count():
-            self.turn = 0
-            self.end_round()
-
-        else:
-            next_player = self.player_set.get(position=self.order.split(',')[self.turn])
-            next_player.turn = True
-            next_player.save()
-
         self.save()
 
 
@@ -164,7 +218,7 @@ class Player(models.Model):
     game = models.ForeignKey('game.Game')
     user = models.ForeignKey(User)
     points = models.IntegerField(default=0)
-    position = models.IntegerField()
+    position = models.IntegerField(default=0)
     turn = models.BooleanField(default=False)
     action = models.OneToOneField('game.Action', null=True, blank=True)
     error = models.CharField(max_length=100, default="")
@@ -178,11 +232,15 @@ class Player(models.Model):
             hand = Hand(player=self)
             hand.save()
 
-    def reset(self):
+    def reset(self, position=None, turn=False):
         self.points = 0
-        self.turn = False
+        self.turn = turn
         self.error = ""
         self.ready = True
+
+        if position is not None:
+            self.position = position
+
         self.save()
 
     def __str__(self):
@@ -192,6 +250,23 @@ class Player(models.Model):
         self.ready = True
         self.save()
         self.game.start_round()
+
+    def set_turn(self, turn):
+        self.turn = turn
+        self.save()
+
+    def end_round(self):
+        if not self.action.face_up:
+            self.action.face_up = True
+            self.action.save()
+
+        self.ready = False  # Used to trigger next round deal
+        self.save()
+
+    def won_round(self, points):
+        self.points += points
+        self.turn = True  # Round winner starts next hand
+        self.save()
 
     def cards_left(self):
         return self.hand.cards.count() + self.hand.selected.count()
